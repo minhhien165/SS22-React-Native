d6f9451cd46952d9adc26532e3ebd3daf8adfa72
minhhien165 <147390428+minhhien165@users.noreply.github.com>
2025-10-29 10:04:27 +0700

    Add files via upload

--- front/utils/axios-instance.ts
+++ front/utils/axios-instance.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.15:3000/api/v1";
+const BASE_URL = "http://192.168.4.100:3000/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   timeout: 10000,
+
 });
