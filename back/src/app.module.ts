739315b2fb82a8581ca18c2d60f3f04df22817d8
minhhien165 <147390428+minhhien165@users.noreply.github.com>
2025-10-29 09:55:23 +0700

    Add files via upload

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -9,8 +9,19 @@ import { TaskModule } from './tasks/task.module';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '3306', 10),
       username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || '123456',
-      database: process.env.DB_NAME || 'my_motherfucker_todolist',
+      password: process.env.DB_PASS || 'hien123{ Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskModule } from './tasks/task.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'hien123',
+      database: process.env.DB_NAME || 'Mobile_todolist',
       autoLoadEntities: true,
       synchronize: true,
       logging: false,
@@ -19,3 +30,4 @@ import { TaskModule } from './tasks/task.module';
   ],
 })
 export class AppModule {}
+
